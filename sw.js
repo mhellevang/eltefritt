d689017d109d658b17cfe899dbb2e75f83c3b0b7
Mathias Hellevang <mathias.hellevang@gmail.com>
2026-08-16 19:57:06 +0200

    Aktiver Web Push mot deployet push-server
    Fyller inn PUSH_SERVER (eltefritt-push.mathias-hellevang.workers.dev)
    og offentlig VAPID-nøkkel; workeren er deployet og svarer. Bumper
    CACHE_VERSION til v21 så installerte klienter får aktiveringen straks.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Eltefritt service worker
 // Bump CACHE_VERSION when you deploy changes you want clients to pick up immediately.
-const CACHE_VERSION = 'eltefritt-v20';
+const CACHE_VERSION = 'eltefritt-v21';
 
 const CORE_ASSETS = [
   './',
